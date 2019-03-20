559a2e77a024a48d67602879c051f724fc349b20
Raigyo <vincent.chilot@gmail.com>
2019-03-20 16:09:48 +0100

    Final Ok

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/index.css';
 
-
 // On ajoute le paramètre title à notre component
 ReactDOM.render(
     <App title = "HappyDrink"/>,
