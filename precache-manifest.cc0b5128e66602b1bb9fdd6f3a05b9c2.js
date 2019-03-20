641afebc7a67101170dfadb56e481c3abacbd414
Raigyo <vincent.chilot@gmail.com>
2019-03-20 16:33:48 +0100

    Updates

---
+++ precache-manifest.cc0b5128e66602b1bb9fdd6f3a05b9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/happydrink/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7e30ab0261c2526cb0e",
+    "url": "/happydrink/static/js/runtime~main.67011007.js"
+  },
+  {
+    "revision": "c2132acb768100defcd9",
+    "url": "/happydrink/static/js/main.8c6f7217.chunk.js"
+  },
+  {
+    "revision": "157f51664789baf60b64",
+    "url": "/happydrink/static/js/2.1279535a.chunk.js"
+  },
+  {
+    "revision": "c2132acb768100defcd9",
+    "url": "/happydrink/static/css/main.a53af98b.chunk.css"
+  },
+  {
+    "revision": "ee3cc4ad7d678f2813144ef2f68a0098",
+    "url": "/happydrink/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b03ea430057b16f36f66787ed9f543d0.js"
+  "/happydrink/precache-manifest.cc0b5128e66602b1bb9fdd6f3a05b9c2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/happydrink/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
