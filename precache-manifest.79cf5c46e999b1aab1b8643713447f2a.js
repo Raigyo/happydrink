b8f5ccbeefcdfd673736287c695ca4e6689e99b8
Raigyo <vincent.chilot@gmail.com>
2019-03-26 09:46:10 +0100

    Updates

--- precache-manifest.79cf5c46e999b1aab1b8643713447f2a.js
+++ precache-manifest.79cf5c46e999b1aab1b8643713447f2a.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/Raigyo/happydrink/static/js/runtime~main.d3f78dd5.js"
   },
   {
-    "revision": "c2132acb768100defcd9",
-    "url": "/Raigyo/happydrink/static/js/main.8c6f7217.chunk.js"
+    "revision": "13fd15263db5a32fa0a6",
+    "url": "/Raigyo/happydrink/static/js/main.5f283016.chunk.js"
   },
   {
-    "revision": "157f51664789baf60b64",
-    "url": "/Raigyo/happydrink/static/js/2.1279535a.chunk.js"
+    "revision": "2efc161a9861cfaaf948",
+    "url": "/Raigyo/happydrink/static/js/2.69395e53.chunk.js"
   },
   {
-    "revision": "c2132acb768100defcd9",
+    "revision": "13fd15263db5a32fa0a6",
     "url": "/Raigyo/happydrink/static/css/main.a53af98b.chunk.css"
   },
   {
-    "revision": "a207b2692c00295f0b41e1e9fe425eab",
+    "revision": "0c38b725d0f8301ca7ebfef53dd79936",
     "url": "/Raigyo/happydrink/index.html"
   }
 ];
